1849fb3ca6eb5b04bd795b3ec3b724b67e26d201
Waldemar Woźniak <wozniakvsc@gmail.com>
2020-07-18 20:13:34 +0200

    val to per

--- AirMonitor/AirMonitor/App.xaml.cs
+++ AirMonitor/AirMonitor/App.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using AirMonitor.Views;
 
 namespace AirMonitor
 {
@@ -10,7 +11,7 @@ namespace AirMonitor
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new RootTabbedPage();
         }
 
         protected override void OnStart()
